9d17cc51d80884255844db7be45f581363870cfc
bryan-kwan <bryan.kwan.mail@gmail.com>
2022-03-15 11:53:47 -0600

    Add 343 section

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,5 +1,6 @@
 
 const Projects = [ //title, description, image src, page number --strings
+    ["ENEL343", "This course taught me about circuit analysis", "343.png", "5"],
     ["Python Chess Game", "Simple chess game with legal move logic. The thumbnail features the game Kholmov vs Bronstein from the 1964 USSR Championship. White to play: can you find the best move?", "kholmov_chess_1.png", "4"],
     ["Route Optimization Script", "This is a route optimization script that I developed for my engineering club, SUAV, in our Aeroconnect 2021 competition.", "RouteOptimizationThumbnail.png", "1"],
     ["ENCM369", "In this course I learned the AVR microarchitecture and how to read a data sheet", "ENCM369_1.jpg", "3"],
